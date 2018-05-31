206b1c0fd05762113b34bd41fa4ec0280389bca4
Mattia Babbini <mattiababbini99@gmail.com>
2018-05-31 23:18:09 +0200

    Aggiunto login
    Integrazione menu di login,
    github di @laur1s

--- index.php
+++ index.php
@@ -4,121 +4,157 @@
 ob_start();
 session_start();
 require_once 'model/dbconnect.php';
-$login = true;
+$login = false; //default false
+
+//controllo se l'utente ha già fatto il log in
 if (isset($_SESSION['user'])) {
-    $login = true;
+    $login = true; //cambio status del login in true (utente loggato)
     
 
 // select logged in users detail
-$res = $conn->query("SELECT * FROM users WHERE id=" . $_SESSION['user']);
-$userRow = mysqli_fetch_array($res, MYSQLI_ASSOC);
+$res = $conn->query("SELECT * FROM users WHERE id=" . $_SESSION['user']); //trovo l'utente nel database
+$userRow = mysqli_fetch_array($res, MYSQLI_ASSOC); //restituisco la riga sottoforma di array associativo 
 }
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>Humystop</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"/>
-<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+  <title>Humystop</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"/>
+  <link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
 </head>
 <body id="top">
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- Top Background Image Wrapper -->
-<div class="bgded overlay" style="background-image:url('images/demo/backgrounds/01.png');"> 
   <!-- ################################################################################################ -->
-  <div class="wrapper row1">
-    <header id="header" class="hoc clear"> 
-      <!-- ################################################################################################ -->
-      <div id="logo" class="fl_left">
-        <h1><a href="index.html">Overerts</a></h1>
-      </div>
-      <nav id="mainav" class="fl_right">
-        <ul class="clear">
-          <li class="active"><a href="index.html">Home</a></li>
-          <li><a class="drop" href="#">Pages</a>
-            <ul>
-              <li><a href="pages/gallery.html">Gallery</a></li>
-              <li><a href="pages/full-width.html">Full Width</a></li>
-              <li><a href="pages/sidebar-left.html">Sidebar Left</a></li>
-              <li><a href="pages/sidebar-right.html">Sidebar Right</a></li>
-              <li><a href="pages/basic-grid.html">Basic Grid</a></li>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- Immagine di background -->
+  <div class="bgded overlay" style="background-image:url('images/demo/backgrounds/01.png');"> 
+    <!-- ################################################################################################ -->
+    <div class="wrapper row1">
+      <header id="header" class="hoc clear"> 
+        <!-- ################################################################################################ -->
+        <div id="logo" class="fl_left">
+          <h1><a href="index.html">Overerts</a></h1>
+        </div>
+        <nav id="mainav" class="fl_right">
+          <ul class="clear">
+            <li class="active"><a href="index.html">Home</a></li>
+            <li><a class="drop" href="#">Pages</a>
+              <ul>
+                <li><a href="pages/gallery.html">Gallery</a></li>
+                <li><a href="pages/full-width.html">Full Width</a></li>
+                <li><a href="pages/sidebar-left.html">Sidebar Left</a></li>
+                <li><a href="pages/sidebar-right.html">Sidebar Right</a></li>
+                <li><a href="pages/basic-grid.html">Basic Grid</a></li>
+              </ul>
+            </li>
+            <li><a class="drop" href="#">Dropdown</a>
+              <ul>
+                <li><a href="#">Level 2</a></li>
+                <li><a class="drop" href="#">Level 2 + Drop</a>
+                  <ul>
+                    <li><a href="#">Level 3</a></li>
+                    <li><a href="#">Level 3</a></li>
+                    <li><a href="#">Level 3</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Level 2</a></li>
+              </ul>
+            </li>
+            <?php 
+		  if(!$login){ //se l'utente NON ha ancora effettuato il login 
+		  
+		  //-------------------------------------------stampa bottoni per login-----------------------------------------
+     echo'
+     <li><a href="pages/login.php">Login </a></li>
+     <li><a href="pages/register.php">registrati </a></li>
+
+     ';};
+		  // ------------------------------------------------- FINE ----------------------------------------------------
+     ?>
+
+     <?php
+
+		  if($login){//se l'utente ha effettuato l'accesso
+		  
+			  //-------------------------------- stampa dati essenziali + bottone logout ---------------------------------
+     echo '
+
+
+     <li class="dropdown">
+     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+     aria-expanded="false">
+     <span
+     class="glyphicon glyphicon-user"></span>&nbsp;Logged
+     in:  echo $userRow["email"]; 
+     &nbsp;<span class="caret"></span></a>
+     <ul class="dropdown-menu">
+     <li><a href="logout.php?logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Logout</a>
+     </li>
+     </ul>
+     </li>
+     ';};
+			// ------------------------------------------------- FINE ----------------------------------------------------
+     ?>
+
+   </ul>
+ </nav>
+ <!-- ################################################################################################ -->
+</header>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div id="pageintro" class="hoc clear"> 
+  <!-- ################################################################################################ -->
+  <div class="flexslider basicslider">
+    <ul class="slides">
+      <li>
+        <article>
+          <p class="heading">Cum sociis natoque penatibus</p>
+          <h2 class="heading">Tincidunt nec venenatis etiam tellus</h2>
+          <p>Et magnis dis montes ridiculus mus sed mi eros molestie eget mauris</p>
+          <footer>
+            <ul class="nospace inline pushright">
+              <li><a class="btn" href="#">Parturient</a></li>
+              <li><a class="btn inverse" href="#">Nascetur</a></li>
             </ul>
-          </li>
-          <li><a class="drop" href="#">Dropdown</a>
-            <ul>
-              <li><a href="#">Level 2</a></li>
-              <li><a class="drop" href="#">Level 2 + Drop</a>
-                <ul>
-                  <li><a href="#">Level 3</a></li>
-                  <li><a href="#">Level 3</a></li>
-                  <li><a href="#">Level 3</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Level 2</a></li>
+          </footer>
+        </article>
+      </li>
+      <li>
+        <article>
+          <p class="heading">Urna gravida eget consequat</p>
+          <h2 class="heading">Rhoncus pharetra ligula vestibulum</h2>
+          <p>Sed varius dui eget convallis nibh lectus ultricies lacus ac auctor lacus</p>
+          <footer>
+            <ul class="nospace inline pushright">
+              <li><a class="btn" href="#">Consequat</a></li>
+              <li><a class="btn inverse" href="#">Phasellus</a></li>
             </ul>
-          </li>
-          <li><a href="#">Link Text</a></li>
-		  <?php ?>
-        </ul>
-      </nav>
-      <!-- ################################################################################################ -->
-    </header>
+          </footer>
+        </article>
+      </li>
+      <li>
+        <article>
+          <p class="heading">Porta congue lacus eleifend</p>
+          <h2 class="heading">Efficitur porta quisque nisl odio suscipit</h2>
+          <p>Ante et velit in elit sapien vulputate non mattis ut euismod sed nisi</p>
+          <footer>
+            <ul class="nospace inline pushright">
+              <li><a class="btn" href="#">Accumsan</a></li>
+              <li><a class="btn inverse" href="#">Molestie</a></li>
+            </ul>
+          </footer>
+        </article>
+      </li>
+    </ul>
   </div>
   <!-- ################################################################################################ -->
-  <!-- ################################################################################################ -->
-  <!-- ################################################################################################ -->
-  <div id="pageintro" class="hoc clear"> 
-    <!-- ################################################################################################ -->
-    <div class="flexslider basicslider">
-      <ul class="slides">
-        <li>
-          <article>
-            <p class="heading">Cum sociis natoque penatibus</p>
-            <h2 class="heading">Tincidunt nec venenatis etiam tellus</h2>
-            <p>Et magnis dis montes ridiculus mus sed mi eros molestie eget mauris</p>
-            <footer>
-              <ul class="nospace inline pushright">
-                <li><a class="btn" href="#">Parturient</a></li>
-                <li><a class="btn inverse" href="#">Nascetur</a></li>
-              </ul>
-            </footer>
-          </article>
-        </li>
-        <li>
-          <article>
-            <p class="heading">Urna gravida eget consequat</p>
-            <h2 class="heading">Rhoncus pharetra ligula vestibulum</h2>
-            <p>Sed varius dui eget convallis nibh lectus ultricies lacus ac auctor lacus</p>
-            <footer>
-              <ul class="nospace inline pushright">
-                <li><a class="btn" href="#">Consequat</a></li>
-                <li><a class="btn inverse" href="#">Phasellus</a></li>
-              </ul>
-            </footer>
-          </article>
-        </li>
-        <li>
-          <article>
-            <p class="heading">Porta congue lacus eleifend</p>
-            <h2 class="heading">Efficitur porta quisque nisl odio suscipit</h2>
-            <p>Ante et velit in elit sapien vulputate non mattis ut euismod sed nisi</p>
-            <footer>
-              <ul class="nospace inline pushright">
-                <li><a class="btn" href="#">Accumsan</a></li>
-                <li><a class="btn inverse" href="#">Molestie</a></li>
-              </ul>
-            </footer>
-          </article>
-        </li>
-      </ul>
-    </div>
-    <!-- ################################################################################################ -->
-  </div>
-  <!-- ################################################################################################ -->
+</div>
+<!-- ################################################################################################ -->
 </div>
 <!-- End Top Background Image Wrapper -->
 <!-- ################################################################################################ -->
@@ -288,96 +324,6 @@ $userRow = mysqli_fetch_array($res, MYSQLI_ASSOC);
     <!-- ################################################################################################ -->
   </section>
 </div>
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<div class="wrapper row4">
-  <footer id="footer" class="hoc clear"> 
-    <!-- ################################################################################################ -->
-    <div class="one_third first">
-      <h6 class="heading">est ut dolor tristique</h6>
-      <p>maecenas tempus vestibulum felis in efficitur sed facilisis urna metus interdum pretium mi dignissim et fusce.</p>
-      <p class="btmspace-15">sagittis tempor nullam iaculis dolor id condimentum cursus duis scelerisque ac metus amet laoreet vestibulum dictum.</p>
-      <ul class="faico clear">
-        <li><a class="faicon-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-        <li><a class="faicon-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-        <li><a class="faicon-dribble" href="#"><i class="fa fa-dribbble"></i></a></li>
-        <li><a class="faicon-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
-        <li><a class="faicon-google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
-        <li><a class="faicon-vk" href="#"><i class="fa fa-vk"></i></a></li>
-      </ul>
-    </div>
-    <div class="one_third">
-      <h6 class="heading">luctus vestibulum magna</h6>
-      <ul class="nospace linklist contact">
-        <li><i class="fa fa-map-marker"></i>
-          <address>
-          Street Name &amp; Number, Town, Postcode/Zip
-          </address>
-        </li>
-        <li><i class="fa fa-phone"></i> +00 (123) 456 7890</li>
-        <li><i class="fa fa-fax"></i> +00 (123) 456 7890</li>
-        <li><i class="fa fa-envelope-o"></i> info@domain.com</li>
-      </ul>
-    </div>
-    <div class="one_third">
-      <h6 class="heading">velit porttitor ac euismod</h6>
-      <ul class="nospace linklist">
-        <li>
-          <article>
-            <h2 class="nospace font-x1"><a href="#">ut porttitor sit amet</a></h2>
-            <time class="font-xs block btmspace-10" datetime="2045-04-06">Friday, 6<sup>th</sup> April 2045</time>
-            <p class="nospace">nunc sed eget augue varius dapibus mi eget lobortis risus nunc a leo&hellip;</p>
-          </article>
-        </li>
-        <li>
-          <article>
-            <h2 class="nospace font-x1"><a href="#">finibus commodo ex eu</a></h2>
-            <time class="font-xs block btmspace-10" datetime="2045-04-05">Thursday, 5<sup>th</sup> April 2045</time>
-            <p class="nospace">pharetra nam sit amet lacus tempor ipsum finibus luctus sed fringilla&hellip;</p>
-          </article>
-        </li>
-      </ul>
-    </div>
-    <!-- ################################################################################################ -->
-  </footer>
-</div>
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<div class="wrapper quicklinks">
-  <nav class="hoc clear"> 
-    <!-- ################################################################################################ -->
-    <ul class="nospace">
-      <li><a href="#"><i class="fa fa-lg fa-home"></i></a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Contact</a></li>
-      <li><a href="#">Blog</a></li>
-      <li><a href="#">FAQ</a></li>
-      <li><a href="#">Shop</a></li>
-    </ul>
-    <!-- ################################################################################################ -->
-  </nav>
-</div>
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<div class="wrapper row5">
-  <div id="copyright" class="hoc clear"> 
-    <!-- ################################################################################################ -->
-    <p class="fl_left">Copyright &copy; 2016 - All Rights Reserved - <a href="#">Humystop s.r.l</a></p>
-
-    <!-- ################################################################################################ -->
-  </div>
-</div>
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
-<!-- JAVASCRIPTS -->
-<script src="layout/scripts/jquery.min.js"></script>
-<script src="layout/scripts/jquery.backtotop.js"></script>
-<script src="layout/scripts/jquery.mobilemenu.js"></script>
-<script src="layout/scripts/jquery.flexslider-min.js"></script>
-</body>
-</html>
+<?php
+        require __DIR__ . "/pages/bottom.php"
+        ?>
